f12540ea688d343c8c534d9d833b24e5171373a4
Navneet <navn0306@gmail.com>
2021-11-26 02:06:07 +0530

    Add: Added Random Number Generator

--- src/com/bridge/SnakeLadder/SnakeLadder.java
+++ src/com/bridge/SnakeLadder/SnakeLadder.java
@@ -4,7 +4,11 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
         int position = 0 ;
-        System.out.println(" Position of Player is :" +position);
+        int Min = 1, Max = 6;
 
+        double number =  Min + (int)(Math.random() * (( Max - Min) + 1));
+
+        System.out.println(" Position of Player is :" +position);
+        System.out.println(" Player got the number is :" +number);
     }
 }
