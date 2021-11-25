78d40dfa5a65d461e5b2b8fc99a66eb89f47b7d8
Navneet <navn0306@gmail.com>
2021-11-26 01:44:59 +0530

    Add: Added Player Position

---
+++ src/com/bridge/SnakeLadder/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridge.SnakeLadder;
+
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+        int position = 0 ;
+        System.out.println(" Position of Player is :" +position);
+
+    }
+}
