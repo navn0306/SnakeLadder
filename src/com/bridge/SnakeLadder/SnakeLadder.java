227e2c78b0650465fb7e58a19eafec70a0c61494
Navneet <navn0306@gmail.com>
2021-11-26 14:52:17 +0530

    Add : Added option check for Ladder, Snake, No Play

--- src/com/bridge/SnakeLadder/SnakeLadder.java
+++ src/com/bridge/SnakeLadder/SnakeLadder.java
@@ -2,12 +2,24 @@ package com.bridge.SnakeLadder;
 
 public class SnakeLadder {
 
+    public static final int Start_Position = 0;
+    public static final int IS_SNAKE = 1;
+    public static final int IS_LADDER = 2;
+
     public static void main(String[] args) {
-        int Start_Position = 0;
 
+        int currentPostion = 0;
         int Dice_Number = (int) (Math.random() * 6 + 1);
+        int option = (int) ((Math.random() * 10 % 3) + 1);
 
-        System.out.println(" Position of Player is :" + Start_Position);
-        System.out.println(" Player got the number is :" + Dice_Number);
+        if (option == IS_SNAKE) {
+            currentPostion -= IS_SNAKE;
+        } else if (option == IS_LADDER) {
+            currentPostion += Dice_Number;
+        } else {
+            System.out.println("No Play");
+        }
+        System.out.println("Curretn Position : " +currentPostion);
+
     }
 }
